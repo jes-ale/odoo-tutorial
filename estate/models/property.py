3591d79cae3f2e437392230c72ef97dfa34a3753
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-13 11:00:10 -0700

    Tutorial v6.4

--- estate/models/property.py
+++ estate/models/property.py
@@ -4,13 +4,13 @@ class TestModel(models.Model):
     _name = "estate_property"
     _description = "Test Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(default="Unknown", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + timedelta(days=90), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()

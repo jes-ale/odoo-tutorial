8e3226daceaea77886c53c795304c79f8bcbf50e
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-15 09:31:07 -0700

    Tutorial v8.2

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -4,7 +4,7 @@ class PropertyTypeModel(models.Model):
     _name = "estate_property_type"
     _description = "Type property"
 
-    name = fields.char(default="Unknown", required=True)
+    name = fields.Char(default="Unknown", required=True)
     property_type = fields.Selection(
         string='Type',
         selection=[('house','House'), ('apartment','Apartment'), ('infonavit','Infonavit')]

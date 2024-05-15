cf13d05907886233e817630ebd7c51f2e95f609f
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-15 13:02:24 -0700

    Tutorial v8.3.12

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,5 +1,6 @@
-from . import property
+from . import estate_property
 from . import property_type
 from . import property_tags
 from . import res_partners
 from . import res_users
+from . import offer

e850571a515f93175fd121bad005f5ce753c20b9
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-15 12:21:01 -0700

    Tutorial v8.3.4

--- estate/models/res_partners.py
+++ estate/models/res_partners.py
@@ -4,5 +4,3 @@ from odoo import fields, models
 class Partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
-
-    offer_ids = fields.One2many("property_offer", "partner_id", string='Offer')

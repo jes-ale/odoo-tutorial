a6b92b89d089b48bffb78fc582719226ded5b335
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-20 09:32:32 -0700

    Tutorial v9.1.5

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -16,3 +16,22 @@ class OfferModel(models.Model):
         required=True
     )
     property_id = fields.Many2one('estate_property', string="property", required=True, index=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='')
+    create_date = fields.Date(string="Creation Date", readonly=True)
+
+    @api.depends('create_date', 'validity')
+        def _compute_date_deadline(self):
+            for offer in self:
+                if offer.create_date:
+                    offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+                else:
+                    offer.date_deadline = False
+
+        def _inverse_date_deadline(self):
+            for offer in self:
+                if offer.create_date and offer.date_deadline:
+                    delta = offer.date_deadline - offer.create_date.date()
+                    offer.validity = delta.days
+                else:
+                    offer.validity = 0

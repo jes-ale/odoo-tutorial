9b8f35077115f0aea0ea50b59014f57ae8d303ee
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-24 10:18:51 -0700

    Tutorial v13.3.1

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,3 +3,10 @@ from odoo import fields, models
 
 class Users(models.Model):
     _inherit = 'res.users'
+
+    property_ids = fields.One2many(
+        'estate_property',
+        'user_id',
+        string='Properties',
+        domain=[('state', '=', 'available')]
+        )

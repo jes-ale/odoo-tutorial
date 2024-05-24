4d434123bab78cfefc2d0fae4f0ea8b0f2d10010
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-24 12:27:50 -0700

    t

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.1',
     'description': "Este es un modulo de pruebas.", 
     'application': True,
+    'depends': ['res.users', 'estate_property'],
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',

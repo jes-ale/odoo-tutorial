97919608f15fde0ca11870fef0ca135683ad9b23
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-24 11:46:31 -0700

    t

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,8 +2,7 @@ from odoo import fields, models
 
 
 class Users(models.Model):
-    _name = 'res.users'
-    _inherit = 'hr.employee'
+    _inherit = 'res.users'
 
     property_ids = fields.One2many(
         'estate_property',

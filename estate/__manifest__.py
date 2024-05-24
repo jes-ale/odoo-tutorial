853c295b1afac4716cfa2dcb9cf2a26f74d1a452
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-24 10:26:55 -0700

    Tutorial v13.3.4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,7 @@
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tags_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_menus.xml',
         'views/res_user_views.xml'
         
     ]

eada3ed8f353790f598646e1aa63d084160a637d
YaedQuadro <161377150+YaedQuadro@users.noreply.github.com>
2024-05-27 10:33:01 -0700

    T

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 class PropertyModel(models.Model):
-    _inherit = 'estate_property'
+    _inherit = 'estate.property'
 
     @api.model
     def action_sold(self):
